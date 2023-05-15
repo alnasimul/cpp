b51c9cde6cfb2790d5ba40566fa969e8470488ed
alnasimul <alnasimul@gmail.com>
2023-05-15 16:37:35 +0600

    default sorting function c++

---
+++ basic/sort.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int a[5] = { 5, 4, 3, 2, 1 }, n = 5;
+
+    // bulit in sort function for c++
+    
+    sort( a, a+n );
+
+    for( int i=0 ; i<n ; i++){
+        cout << a[i] << " ";
+    }
+
+    return 0;
+}
